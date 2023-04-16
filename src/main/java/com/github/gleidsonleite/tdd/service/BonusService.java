c0bb308714a8a2c37a830e1e24e4b5b1a014add6
Gleidson Leite <gledson.leytte@hotmail.com>
2023-04-16 17:52:23 -0300

    refactoring

--- src/main/java/com/github/gleidsonleite/tdd/service/BonusService.java
+++ src/main/java/com/github/gleidsonleite/tdd/service/BonusService.java
@@ -5,12 +5,15 @@ import com.github.gleidsonleite.tdd.modelo.Funcionario;
 import java.math.BigDecimal;
 
 public class BonusService {
-    public BigDecimal calcularBonus(Funcionario funcionario) {
-        BigDecimal valor = funcionario.getSalario().multiply(
-                new BigDecimal("0.1")
-        );
 
-        if(valor.compareTo(new BigDecimal("1000")) > 0) {
+    private static final BigDecimal LIMITE_BONUS = new BigDecimal("1000");
+
+    private static final BigDecimal PERCENTUAL_BONUS = new BigDecimal("0.1");
+
+    public BigDecimal calcularBonus(Funcionario funcionario) {
+        BigDecimal valor = funcionario.getSalario().multiply(PERCENTUAL_BONUS);
+
+        if(valor.compareTo(LIMITE_BONUS) > 0) {
             valor = BigDecimal.ZERO;
         }
 
